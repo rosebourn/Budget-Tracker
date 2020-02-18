a1ed48232759e1ed80060872bfb98d512edb47ca
mianak78 <rosebourn@gmail.com>
2020-02-17 18:14:55 -0700

    heroku attempt

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+
 
 const app = express();
 
